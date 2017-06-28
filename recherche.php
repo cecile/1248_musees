4964cafff2f2c1936f32ca39bca592225d4521f5
alesksela <alexia.magnaldi@gmail.com>
2017-06-28 11:57:46 +0200

    28/06 11h57

--- recherche.php
+++ recherche.php
@@ -74,7 +74,7 @@ if(!empty($motCle)){
 
         
       
-                   <div class="card horizontal col s12 m4 l4 xl4 hop">
+                   <div class="card horizontal col s12 m4 l4 xl4 hop ">
                       <div class="card-stacked hoverable">
                         <div class="card-content">
                     <h2 class="hc">
